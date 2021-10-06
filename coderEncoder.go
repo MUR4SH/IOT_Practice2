9ab7d7a490640bed6348d5bc9d13db096999674c
Alex <49249097+MUR4SH@users.noreply.github.com>
2021-10-06 18:17:29 +0300

    to string
    крч я забил:
     - все функции кодирования возвращают строку
     - все функции декодирования принимают строку

--- coderEncoder.go
+++ coderEncoder.go
@@ -16,21 +16,21 @@ type Format struct {
 	Hash      string
 }
 
-// Кодирует трое данных в json (возвращает массив byte)
-func JSON_encode(r_id uint, r_time string, r_hash string) []byte {
+// Кодирует трое данных в json
+func JSON_encode(r_id uint, r_time string, r_hash string) string {
 	g_json, err := json.Marshal(Format{r_id, r_time, r_hash}) // Создаем объект и делаем из него json
 	if err != nil {
 		log.Fatal(err)
 	}
-	return g_json
+	return string(g_json)
 }
 
-func XML_encode(r_id uint, r_time string, r_hash string) []byte {
+func XML_encode(r_id uint, r_time string, r_hash string) string {
 	g_xml, err := xml.Marshal(Format{r_id, r_time, r_hash})
 	if err != nil {
 		log.Fatal(err)
 	}
-	return g_xml
+	return string(g_xml)
 }
 
 /*
@@ -40,7 +40,6 @@ func XML_encode(r_id uint, r_time string, r_hash string) []byte {
 	<span id='timestamp'><span>
 	<span id='hash'><span>
 </div>
-Возвращает строку
 */
 func HTML_encode(r_id uint, r_time string, r_hash string) string {
 	var html string = "<div id='html_data'>"
@@ -51,25 +50,24 @@ func HTML_encode(r_id uint, r_time string, r_hash string) string {
 }
 
 // Декодирует json в трое данных - id, timestamp, hash (их и возвращает - int, string, string)
-func JSON_decode(r_json []byte) (uint, string, string) {
+func JSON_decode(r_json string) (uint, string, string) {
 	var m Format //Создаем переменную, в которую будет записан расшифрованный json
-	err := json.Unmarshal(r_json, &m)
+	err := json.Unmarshal([]byte(r_json), &m)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return m.Id, m.Timestamp, m.Hash
 }
 
-func XML_decode(r_xml []byte) (uint, string, string) {
+func XML_decode(r_xml string) (uint, string, string) {
 	var m Format
-	err := xml.Unmarshal(r_xml, &m)
+	err := xml.Unmarshal([]byte(r_xml), &m)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return m.Id, m.Timestamp, m.Hash
 }
 
-//Принимает строку
 func HTML_decode(html string) (int, string, string) {
 	var id int
 	var time, hash string
